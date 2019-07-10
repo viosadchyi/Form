e79a3d3a2bd36daae8f3b609505cc3d90a4eeab8
viosadchyi <slavosadchy@gmail.com>
2019-07-10 12:12:16 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,25 @@
+var button = document.getElementById( 'button' );
+button.addEventListener ('click', saySmth);
+function saySmth(event) {
+    console.log (event);
+    var firstName = document.getElementById('firstname').value;
+    var lastName = document.getElementById('lastname').value;
+    var email = document.getElementById('email').value;
+    if (firstName.length < 5) {
+        alert ('First name is too short');
+    } 
+    if (lastName.length < 5) {
+    alert ('Last name is too short');
+    }
+    if (email.length < 5) {
+    alert ('Email is too short');
+    } else {
+    alert ('Success!');
+    }
+} 
+var user1 = {
+    firstName: firstName,
+    lastName: lastName,
+    email: email
+};
+console.log (user1);
